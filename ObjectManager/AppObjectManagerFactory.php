8d319d16e02dee15ecf01836b16c436b1de09391
Thomas Klein <thomasklein876@gmail.com>
2025-10-10 11:34:32 +0200

    Fix namespace

--- ObjectManager/AppObjectManagerFactory.php
+++ ObjectManager/AppObjectManagerFactory.php
@@ -5,10 +5,9 @@
  */
 declare(strict_types=1);
 
-namespace Opengento\Application;
+namespace Opengento\Application\ObjectManager;
 
 use Magento\Framework\App\ObjectManagerFactory;
-use Opengento\Application\AppObjectManager;
 
 class AppObjectManagerFactory extends ObjectManagerFactory
 {
